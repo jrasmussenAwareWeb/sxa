3bf8fffad8bb64167127def8d72e407bed642a1f
Jen Rasmussen <jrasmussen@awareweb.com>
2017-06-25 22:25:41 -0500

    restructuring

---
+++ js/controllers/componentListCtrl.js
@@ -0,0 +1,1 @@
+sxa.app.controller('ComponentListCtrl', ['$scope', '$routeParams', 'ComponentResources',    function ($scope, $routeParams, ComponentResources) {    $scope.LoadingIndicator = true;    $scope.components = ComponentResources.components;}]);
